e9336b6f7472a0ba7f1a1560efe6f417693cc391
kianaraquel97 <102600192+kianaraquel97@users.noreply.github.com>
2022-03-29 10:16:05 -0400

    Update gitstart.rb

--- gitstart.rb
+++ gitstart.rb
@@ -7,3 +7,23 @@ git remote add origin https://github.com/kianaraquel97/homebrew-repo.git
 git push -u origin main
 git tag -a v0.0.2 -m "version 0.2.0"
 git push origin v0.2.0
+class Gitstart <Formula
+  desc "Gitstart creates, adds, and pushes a new repo with one line."
+  homepage "https://github.com/kianaraquel97/gitstart"
+  url https://github.com/kianaraquel97/homebrew-repo/archive/refs/tags/v0.2.0.tar.gz
+  kianaraquel97 "your-kianaraquel97-value"
+  license "MIT"
+  
+  depends_on "yq"
+  depends_on "gh"
+  depends_on "jq"
+  
+  def install
+    bin.install 'gitstart'
+  end
+end
+git add .
+git commit -m "Version 0.2.0"
+git push
+rm  /opt/homebrew/Library/Taps/homebrew/homebrew-core/Formula/gitstart.rb
+brew tap kianaraquel97/gitstart
